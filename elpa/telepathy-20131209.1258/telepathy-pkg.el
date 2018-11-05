4bbe8685e38f6f52f016ce9c3b4b5ea781efdf87
rookiz <pwnzher001@gmail.com>
2018-11-05 14:29:29 +0800

    rm elpa

--- elpa/telepathy-20131209.1258/telepathy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "telepathy" "20131209.1258" "Access Telepathy from Emacs" 'nil :commit "211d785b02a29ddc254422fdcc3db45262582f8c" :keywords '("telepathy" "tools") :authors '(("Nicolas Petton" . "petton.nicolas@gmail.com")) :maintainer '("Nicolas Petton" . "petton.nicolas@gmail.com"))
